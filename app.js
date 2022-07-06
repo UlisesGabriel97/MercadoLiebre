c9384932b62b54331fece332dd60fc2d1852f764
UlisesGabriel97 <ugmendez97@gmail.com>
2022-07-06 13:30:55 -0300

    Subo los cambios para el TP de Diseño Adaptativo. Ademas subo correcciones de estilos y agrego funcionalidades de Live Reload Server

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-const express=require("express")
+/* Livereload */
+const livereload = require('livereload');
+const liveReloadServer = livereload.createServer();
+
+/* Entry point */
+const express = require('express')
+const connectLivereload = require('connect-livereload')
+const path = require('path')
+
 const app=express()
-const path=require("path")
 const port=3000
 
+/* Archivos estaticos */
 app.use(express.static(path.resolve(__dirname,"public")))
 
+/* Archivos estaticos monitoreados */
+liveReloadServer.watch(path.join(__dirname, 'public'));
+app.use(connectLivereload());
+
+/* Rutas */
 app.get("/",(req,res)=>res.sendFile(path.resolve(__dirname,"views","home.html")))
 
+/* Funcion de actualizacion del servidor */
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
+
+/* Levantamos el servidor con app listen */
 app.listen(port,()=>console.log(`Servidor funcionando en http://localhost:${port}`))
